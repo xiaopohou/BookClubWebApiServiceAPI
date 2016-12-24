86d97e301fb2ee8b78c6cf14e18b432b844d0d36
dpetla <dpetla@DESKTOP-NUK1HLT>
2016-12-24 01:55:57 -0500

    Add project files.

---
+++ FinalProjectAPI/Controllers/BooksController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FinalProjectAPI.Controllers
+{
+    public class BooksController : ApiController
+    {
+        BookDBDataContext db = new BookDBDataContext();
+        // GET: api/Books
+        public IEnumerable<Book> Get()
+        {
+            var myBooks = db.Books;
+            return myBooks;
+        }
+
+        // GET: api/Books/5
+        public Book Get(int id)
+        {
+            // get book based on id from linq connection
+            var books = db.Books;
+            var book = from b in books where b.Id == id select b;
+            Book b1 = book.FirstOrDefault();
+
+            return b1;
+        }
+
+        // POST: api/Books
+        public int Post([FromBody]Book book)
+        {
+
+            db.Books.InsertOnSubmit(book);
+            db.SubmitChanges();
+			return book.Id;
+        }
+
+        // PUT: api/Books/5
+        public int Put(int id, [FromBody]Book updatedBook)
+        {
+            var books = db.Books;
+            Book result = new Book();
+            var b = from a in books where a.Id == id select a;
+            foreach (var i in b)
+            {
+                result = i;
+            }
+            result.Name = updatedBook.Name;
+            result.ReleaseDate = Convert.ToDateTime(updatedBook.ReleaseDate);
+            db.SubmitChanges();
+            return result.Id;
+        }
+
+        // DELETE: api/Books/5
+        public void Delete(int id)
+        {
+            var book = from a in db.Books
+                       where a.Id == id
+                       select a;
+            Book b1 = book.FirstOrDefault();
+
+            db.Books.DeleteOnSubmit(b1);
+            db.SubmitChanges();
+        }
+    }
+}
---
+++ FinalProjectAPI/Controllers/ReviewController.cs
@@ -0,0 +1,74 @@
+﻿using FinalProjectAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FinalProjectAPI.Controllers
+{
+
+    public class ReviewController : ApiController
+    {
+        ReviewDBDataContext db = new ReviewDBDataContext();
+
+        // GET: api/Review
+        public IEnumerable<Review> Get()
+        {
+            var reviews = db.Reviews;
+            return reviews;
+        }
+
+        // GET: api/Review/5
+        public Review Get(int id)
+        {
+            var queryReview = from a in db.Reviews
+                            where a.Id == id
+                            select a;
+
+            Review review = queryReview.FirstOrDefault();
+            return review;
+        }
+
+        // POST: api/Review
+        public int Post([FromBody] Review newReview)
+        {
+            newReview.Date = DateTime.Now;
+            db.Reviews.InsertOnSubmit(newReview);
+            db.SubmitChanges();
+            return newReview.Id;
+        }
+
+        // PUT: api/Review/5
+        public int Put(int id, [FromBody]Review updatedReview)
+        {
+            var reviews = db.Reviews;
+            Review result = new Review();
+            var v = from a in reviews where a.Id == id select a;
+            foreach (var i in v)
+            {
+                result = i;
+            }
+            result.Reviewer = updatedReview.Reviewer;
+            result.Review1 = updatedReview.Review1;
+            result.Date = DateTime.Now;
+            result.Rating = updatedReview.Rating;
+            result.BookID = updatedReview.BookID;
+            db.SubmitChanges();
+            return result.Id;
+        }
+
+        // DELETE: api/Review/5
+        public void Delete(int id)
+        {
+            var review = from a in db.Reviews
+                       where a.Id == id
+                       select a;
+            Review target = review.FirstOrDefault();
+
+            db.Reviews.DeleteOnSubmit(target);
+            db.SubmitChanges();
+        }
+    }
+}
